33b83a8c698ecfe0ca51b48bf59cdba0a255dd61
rxxrxxr <wnf2255@gmail.com>
2024-01-22 11:28:25 +0900

    20240122

--- springboot_work/ex0122/src/main/java/org/example/Main3.java
+++ springboot_work/ex0122/src/main/java/org/example/Main3.java
@@ -10,6 +10,8 @@ class SubCls extends SuperCls{
 }
 public class Main3 {
     public static void main(String[] args) {
+        //변수든 메서드든 메모리상에 올려 놓고 써야한다...
+
         System.out.println(SuperCls.count);
         System.out.println(SubCls.count);
 

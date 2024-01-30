ac5d479e6346ba7106749517d0195f1dff33d6a7
rxxrxxr <wnf2255@gmail.com>
2024-01-30 13:04:00 +0900

    20240129

--- springboot_work/miniproject01/src/main/main/java/org/example/DBINFO.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-
-// alt + enter 빠른 에러 처리
-public interface DBINFO {
-    String url = "jdbc:mysql://192.168.0.38:3306/shop01";
-    String user = "pmh";
-    String password = "1234";
-}

4c0f1543066ff033e455e474cf6af3f4ff3260bf
DESKTOP-AGV6E1U\GoChanWoo <wnf2255@gmail.com>
2024-04-11 16:50:31 +0900

    240411

---
+++ springboot_api/mychart/src/main/java/com/gcw/mychart/item/ItemDto.java
@@ -0,0 +1,17 @@
+package com.gcw.mychart.item;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ItemDto {
+
+    private String itemNm;
+    private int price;
+
+}

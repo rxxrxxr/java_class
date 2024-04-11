ba523c7faba41e7e660f983609688f057514b8af
DESKTOP-AGV6E1U\GoChanWoo <wnf2255@gmail.com>
2024-04-11 09:50:57 +0900

    240411

---
+++ springboot_api/mychart/src/test/java/com/gcw/mychart/MychartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gcw.mychart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MychartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
